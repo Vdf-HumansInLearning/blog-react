7f38e31afa754e856d108f2e11c86c3dd3be419c
alexandrumalinescu <almalinescu@yahoo.com>
2022-01-27 17:17:13 +0200

    added Not Found page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./Home";
 import Details from "./routes/Details/Details";
+import NotFound from "./routes/NotFound/NotFound";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/:id" element={<Details />} />
+      <Route path="article/:id" element={<Details />} />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
